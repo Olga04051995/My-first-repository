8e6f10cde830fc3b4d4cd3d6a49a26810af4fcac
olga.lokota <olga_lokota@ukr.net>
2017-06-26 22:35:31 +0300

    get rid of module approach, using class names instead of direct setting styles to an element, remove extra div block for clock-calendar functionality

--- homework2/clockCalendar.js
+++ homework2/clockCalendar.js
@@ -1,119 +1,42 @@
 'use strict';
 
-let clockCalendar = function() {
-
+document.addEventListener('DOMContentLoaded', init, false);
+function init () {
     let clockFormat = 'HH:MM',
         calendarFormat = 'MM/DD/YY',
-        currentState = 'clock';
+        currentState = 'clock',
+        container = document.querySelector('.clock-calendar');
 
-    function init (container, clockElementId, calendarElementId) {
-        let containerElement = document.getElementById(container),
-            clockElement = document.getElementById(clockElementId),
-            calendarElement = document.getElementById(calendarElementId);
+    container.addEventListener ('contextmenu', function (event) {
+        event.preventDefault();
+        currentState = currentState === 'clock' ? 'calendar' : 'clock';
 
-        initBaseContainerEvents(containerElement, clockElement, calendarElement);
-        initializeClock(clockElement);
-        initializeCalendar(calendarElement);
-    }
+        container.innerHTML = getClockCalendarValue(currentState, clockFormat, calendarFormat);
+    });
 
-    function initBaseContainerEvents (containerElement, clockElement, calendarElement) {
-        containerElement.addEventListener("contextmenu", function(event) {
-            currentState = currentState === 'clock' ? 'calendar' : 'clock';
-            toggleState(currentState, clockElement, calendarElement);
-        });
-
-        containerElement.addEventListener("mouseover", function() {
-            containerElement.style.backgroundColor = 'darkgray';
-        });
-
-        containerElement.addEventListener("mouseleave", function() {
-            containerElement.style.backgroundColor = 'black';
-        });
-    }
-
-    function toggleState (state, clockElement, calendarElement) {
-        if (state === 'clock') {
-            calendarElement.style.display = 'none';
-            clockElement.style.display = 'block';
-        } else {
-            clockElement.style.display = 'none';
-            calendarElement.style.display = 'block';
-        }
-    }
-
-    function initializeClock (clockElement) {
-        clockElement.innerHTML = getTime(clockFormat)
-
-        let clockIntervalId = setInterval(function() {
-            clockElement.innerHTML = getTime(clockFormat)
-        }, 1000);
-
-        clockElement.addEventListener("click", function(event) {
+    container.addEventListener ('click', function (event) {
+        if (currentState === 'clock') {
             clockFormat = clockFormat === 'HH:MM' ? 'HH:MM:SS' : 'HH:MM';
-            clockElement.innerHTML = getTime(clockFormat)
-        });
-    }
-
-    function initializeCalendar (calendarElement) {
-        let calendarDayInterval = 60000 * 60 * 24;
-        calendarElement.innerHTML = getDate(calendarFormat);
-
-        let calendarIntervalId = setInterval(function() {
-            calendarElement.innerHTML = getDate(calendarFormat);
-        }, calendarDayInterval);
-
-        calendarElement.addEventListener("click", function(event) {
+        } else if (currentState === 'calendar') {
             calendarFormat = calendarFormat === 'MM/DD/YY' ? 'DD.MM.YYYY' : 'MM/DD/YY';
-            calendarElement.innerHTML = getDate(calendarFormat);
-        });
-    }
-
-    function getTime (format) {
-        let result;
-
-        switch (format) {
-            case "HH:MM":
-                result = new Date().toLocaleTimeString('en-US', {
-                    hour12: false,
-                    hour: "numeric",
-                    minute: "numeric"
-                });
-                break;
-            case "HH:MM:SS":
-                result = new Date().toLocaleTimeString('en-US', {
-                    hour12: false,
-                    hour: "numeric",
-                    minute: "numeric",
-                    second: "numeric"
-                });
-                break;
         }
 
-        return result;
+        container.innerHTML = getClockCalendarValue(currentState, clockFormat, calendarFormat);
+    });
+
+    container.addEventListener ('mouseover', function (event) {
+        container.classList.add ("hover-background");
+    });
+
+    container.addEventListener ('mouseleave', function (event) {
+        container.classList.remove ("hover-background");
+    });
+
+    function updateClockCalendarValue () {
+        container.innerHTML = getClockCalendarValue(currentState, clockFormat, calendarFormat);
     }
 
-    function getDate (format) {
+    updateClockCalendarValue();
+    setInterval(updateClockCalendarValue, 1000);
+}
 
-        let result,
-            today = new Date(),
-            day = today.getDate().toLocaleString('en-US', { minimumIntegerDigits: 2 }),
-            month = (today.getMonth() + 1).toLocaleString('en-US', { minimumIntegerDigits: 2 }),
-            year = today.getFullYear();
-
-        switch (format) {
-            case 'MM/DD/YY':
-                result = month + '/' + day + '/' + year.toString().slice(-2);
-                break;
-            case 'DD.MM.YYYY':
-                result = day + '.' + month + '.' + year;
-                break;
-        }
-
-        return result;
-
-    }
-
-    return {
-        initialize: init
-    }
-}();
---
+++ homework2/helper.js
@@ -0,0 +1,54 @@
+'use strict';
+
+function getClockCalendarValue (currentState, clockFormat, calendarFormat) {
+	let result;
+
+	if (currentState === 'clock') {
+		result = getTime(clockFormat);
+	} else if (currentState === 'calendar') {
+		result = getDate(calendarFormat);
+	}
+	
+	return result;
+}
+
+function getTime (clockFormat) {
+	let result;
+
+	if (clockFormat === 'HH:MM') {
+		result = new Date().toLocaleTimeString('en-US', {
+			hour12: false,
+			hour: "numeric",
+			minute: "numeric"
+		});
+	} else {
+		result = new Date().toLocaleTimeString('en-US', {
+			hour12: false,
+			hour: "numeric",
+			minute: "numeric",
+			second: "numeric"
+		});
+	}
+	
+	return result;
+}
+
+function getDate (calendarFormat) {
+	let result,
+		today = new Date(),
+		day = today.getDate().toLocaleString('en-US', {
+			minimumIntegerDigits: 2
+		}),
+		month = (today.getMonth() + 1).toLocaleString('en-US', {
+			minimumIntegerDigits: 2
+		}),
+		year = today.getFullYear();
+
+	if (calendarFormat === 'MM/DD/YY') {
+		result = month + '/' + day + '/' + year.toString().slice(-2);
+	} else {
+		result = day + '.' + month + '.' + year;
+	}
+
+	return result;
+}
