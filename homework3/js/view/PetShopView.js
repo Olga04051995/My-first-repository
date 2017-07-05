a979bf3c8560a14435194ba2abc93ad1514eedeb
olga.lokota <olga_lokota@ukr.net>
2017-07-05 09:21:54 +0300

    use template in PetShop.js

--- homework3/js/view/PetShopView.js
+++ homework3/js/view/PetShopView.js
@@ -2,6 +2,7 @@
 class PetShopView {
   constructor(model) {
     this.petModel = model;
+    this.template = '';
   }
 
   render(selector) {
@@ -11,11 +12,18 @@ class PetShopView {
     let greaterThanAvaragePrice = this.petModel.getGreaterThanEvaragePrice();
     let fluffyAndWhite = this.petModel.getFluffyAndWhite();
 
-    container.appendChild(this._createListNode('This is all cats', cats));
-    container.appendChild(this._createListNode('All pets with price greater than avarage', greaterThanAvaragePrice));
-    container.appendChild(this._createListNode('All fluffy pets and white color', fluffyAndWhite));
-  }
+    this._createListNode('This is all cats', cats);
+    this._createListNode('All pets with price greater than avarage', greaterThanAvaragePrice);
+    this._createListNode('All fluffy pets and white color', fluffyAndWhite)
 
+    container.innerHTML = this.template;
+
+
+ //   container.appendChild(this._createListNode('This is all cats', cats));
+   // container.appendChild(this._createListNode('All pets with price greater than avarage', greaterThanAvaragePrice));
+    //container.appendChild(this._createListNode('All fluffy pets and white color', fluffyAndWhite));
+  }
+/*
   _createListNode(title, pets) {
     var ul = document.createElement('ul');
     var listHeader = document.createElement('h3');
@@ -29,5 +37,17 @@ class PetShopView {
     });
 
     return ul;
-  }
+  }*/
+    _createListNode (title, pets) {
+        let list = '';
+        var listElement;
+        pets.forEach((value) => {
+        listElement = value.toString();
+        list += `<li>${listElement}</li>`
+        });
+
+        this.template += `<ul><h3>${title}</h3>
+                              ${list}
+                          </ul>`;   
+    } 
 };
